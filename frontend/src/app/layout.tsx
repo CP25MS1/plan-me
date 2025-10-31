af27ffc16b9bca3d5bd835412967fc9c4d58af0f
Tori157 <puttinat2546@gmail.com>
2025-10-31 22:10:56 +0700

    Login , create components , homepage

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,44 +1,32 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Sarabun } from "next/font/google";
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
+const sarabun = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "CP25MS1 Team - Under Construction",
-  description: "Our website is currently under construction. We are working diligently to bring you an amazing experience. Stay tuned for updates!",
+  description:
+    "Our website is currently under construction. We are working diligently to bring you an amazing experience. Stay tuned for updates!",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<head>
-				<meta
-					name='theme-color'
-					content='#25cf7a'
-				/>
-				<link
-					rel='manifest'
-					href='/manifest.webmanifest'
-				/>
-			</head>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        <meta name="theme-color" content="#25cf7a" />
+        <script src="https://cdn.jsdelivr.net/npm/@flaticon/flaticon-uicons@3.3.1/license.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@flaticon/flaticon-uicons@3.3.1/css/all/all.min.css"></link>
+        <link rel="manifest" href="/manifest.webmanifest" />
+      </head>
+      <body className={`${sarabun.variable} antialiased`}>{children}</body>
+    </html>
+  );
 }
---
+++ frontend/src/app/login/actions.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { redirect } from "next/navigation";
+
+export async function startGoogleLogin() {
+  const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
+  const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI!;
+  const SCOPE = ["openid", "email", "profile"].join(" ");
+
+  const params = new URLSearchParams({
+    client_id: GOOGLE_CLIENT_ID,
+    redirect_uri: REDIRECT_URI,
+    response_type: "code",
+    scope: SCOPE,
+    access_type: "offline",
+    prompt: "consent",
+  });
+
+  redirect(`https://accounts.google.com/o/oauth2/v2/auth?${params}`);
+}
