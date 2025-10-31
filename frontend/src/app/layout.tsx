2a257a22d6f7d0cdf00a52e9e0c4507e31819692
ChinnaphatLoha <110705914+ChinnaphatLoha@users.noreply.github.com>
2025-11-01 00:29:56 +0700

    Format code by Prettier

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,16 +1,16 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Sarabun } from "next/font/google";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono, Sarabun } from 'next/font/google';
 
 const sarabun = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "CP25MS1 Team - Under Construction",
+  title: 'CP25MS1 Team - Under Construction',
   description:
-    "Our website is currently under construction. We are working diligently to bring you an amazing experience. Stay tuned for updates!",
+    'Our website is currently under construction. We are working diligently to bring you an amazing experience. Stay tuned for updates!',
 };
 
 export default function RootLayout({
@@ -23,7 +23,10 @@ export default function RootLayout({
       <head>
         <meta name="theme-color" content="#25cf7a" />
         <script src="https://cdn.jsdelivr.net/npm/@flaticon/flaticon-uicons@3.3.1/license.min.js"></script>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@flaticon/flaticon-uicons@3.3.1/css/all/all.min.css"></link>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@flaticon/flaticon-uicons@3.3.1/css/all/all.min.css"
+        ></link>
         <link rel="manifest" href="/manifest.webmanifest" />
       </head>
       <body className={`${sarabun.variable} antialiased`}>{children}</body>
