c0ecc48cc2bfbbffd08e888a57bdb6d202195b03
ChinnaphatLoha <110705914+ChinnaphatLoha@users.noreply.github.com>
2025-11-01 17:03:54 +0700

    Align structure and auth flow with best practices

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,15 @@
-import './globals.css';
+import Script from 'next/script';
+import { Sarabun } from 'next/font/google';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono, Sarabun } from 'next/font/google';
 
-const sarabun = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+import './globals.css';
+import { Providers } from './providers';
+import Navbar from '@/components/navbar';
+
+const sarabun = Sarabun({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+  variable: '--font-sarabun',
+  subsets: ['latin', 'latin-ext', 'thai'],
 });
 
 export const metadata: Metadata = {
@@ -13,23 +18,35 @@ export const metadata: Metadata = {
     'Our website is currently under construction. We are working diligently to bring you an amazing experience. Stay tuned for updates!',
 };
 
-export default function RootLayout({
+const RootLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) => {
   return (
-    <html lang="en">
+    <html lang="th">
       <head>
         <meta name="theme-color" content="#25cf7a" />
-        <script src="https://cdn.jsdelivr.net/npm/@flaticon/flaticon-uicons@3.3.1/license.min.js"></script>
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/@flaticon/flaticon-uicons@3.3.1/css/all/all.min.css"
-        ></link>
+        />
         <link rel="manifest" href="/manifest.webmanifest" />
       </head>
-      <body className={`${sarabun.variable} antialiased`}>{children}</body>
+      <body className={`${sarabun.variable} antialiased`}>
+        <Providers>
+          <div className="flex flex-col min-h-screen">
+            <main className="grow">{children}</main>
+            <Navbar />
+          </div>
+        </Providers>
+        <Script
+          src="https://cdn.jsdelivr.net/npm/@flaticon/flaticon-uicons@3.3.1/license.min.js"
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
