7859edc207ddca382d6bf341f7b229e56474124b
Lalita Benjamanukul <111362962+namfonn02@users.noreply.github.com>
2025-11-08 20:46:12 +0700

    Implement user manages followers and following endpoint
    * Implement User manages followers and following endpoints(endpoint:/users/me/followings, /users/me/followers)
    
    * refactor use actual current user instead of mock user
    
    * Changed the field names and DTO class names to make them clearer.

--- backend/src/main/java/capstone/ms/api/modules/user/controllers/UserController.java
+++ backend/src/main/java/capstone/ms/api/modules/user/controllers/UserController.java
@@ -1,11 +1,11 @@
 package capstone.ms.api.modules.user.controllers;
 
-import capstone.ms.api.modules.user.dto.PreferenceDto;
-import capstone.ms.api.modules.user.dto.UserDto;
+import capstone.ms.api.modules.user.dto.*;
 import capstone.ms.api.modules.user.entities.User;
 import capstone.ms.api.modules.user.services.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -25,9 +25,30 @@ public class UserController {
     }
 
     @GetMapping("/{userId}/profile")
-    public ResponseEntity<UserDto>  getUserProfile(@PathVariable Integer userId) {
+    public ResponseEntity<UserDto> getUserProfile(@PathVariable Integer userId) {
         UserDto userDto = userService.getUserProfile(userId);
         return ResponseEntity.ok(userDto);
+    }
+
+    @PostMapping("/me/following")
+    public ResponseEntity<PublicUserInfo> followUser(@AuthenticationPrincipal User currentUser,
+                                                     @Valid @RequestBody final FollowingIdDto targetUser) {
+        PublicUserInfo followedUser = userService.followUser(currentUser, targetUser.getFollowingId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(followedUser);
+    }
+
+    @DeleteMapping("/me/following")
+    public ResponseEntity<Void> unfollow(@AuthenticationPrincipal User currentUser,
+                                         @Valid @RequestBody final FollowingIdDto targetUser) {
+        userService.unfollowUser(currentUser, targetUser.getFollowingId());
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/me/followers")
+    public ResponseEntity<Void> removeFollower(@AuthenticationPrincipal User currentUser,
+                                               @Valid @RequestBody final FollowerIdDto targetUser) {
+        userService.removeFollower(currentUser, targetUser.getFollowerId());
+        return ResponseEntity.noContent().build();
     }
 
 }
