7377625e37f18d6117c38cef29f7604fec0ba73e
ChinnaphatLoha <110705914+ChinnaphatLoha@users.noreply.github.com>
2025-11-11 00:42:18 +0700

    Fix Frontend build fails

--- frontend/src/app/login/actions.ts
+++ frontend/src/app/login/actions.ts
@@ -1,20 +1,16 @@
-'use server';
+export const startGoogleLogin = () => {
+    const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!;
+    const REDIRECT_URI = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI!;
+    const SCOPE = ['openid', 'email', 'profile'].join(' ');
 
-import { redirect } from 'next/navigation';
+    const params = new URLSearchParams({
+        client_id: GOOGLE_CLIENT_ID,
+        redirect_uri: REDIRECT_URI,
+        response_type: 'code',
+        scope: SCOPE,
+        access_type: 'offline',
+        prompt: 'consent',
+    });
 
-export const startGoogleLogin = async () => {
-  const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
-  const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI!;
-  const SCOPE = ['openid', 'email', 'profile'].join(' ');
-
-  const params = new URLSearchParams({
-    client_id: GOOGLE_CLIENT_ID,
-    redirect_uri: REDIRECT_URI,
-    response_type: 'code',
-    scope: SCOPE,
-    access_type: 'offline',
-    prompt: 'consent',
-  });
-
-  redirect(`https://accounts.google.com/o/oauth2/v2/auth?${params}`);
+    globalThis.location.href = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
 };
--- frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-4xl sm:text-6xl font-bold text-center">
-        CP25MS1 Team was working really hard.
-      </h1>
-    </div>
-  );
-}
