f5b396239303e784f89afe2b1040b9bfdf9ab37d
Chinnaphat Lohasangsuwan <110705914+ChinnaphatLoha@users.noreply.github.com>
2025-11-14 22:30:35 +0700

    MS-007: Implement search user endpoint with Redis caching
    * Implement search user endpoint with Redis caching
    
    * Return empty content for falsy params (search users)

--- backend/src/main/java/capstone/ms/api/modules/user/dto/PublicUserInfo.java
+++ backend/src/main/java/capstone/ms/api/modules/user/dto/PublicUserInfo.java
@@ -1,13 +1,17 @@
 package capstone.ms.api.modules.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class PublicUserInfo {
-    private final Integer id;
-    private final String username;
-    private final String email;
-    private final String profilePicUrl;
+    private Integer id;
+    private String username;
+    private String email;
+    private String profilePicUrl;
 }
