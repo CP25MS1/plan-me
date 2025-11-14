031451624e5ba3049a05f6312f22e38f388ad5f4
Chinnaphat Lohasangsuwan <110705914+ChinnaphatLoha@users.noreply.github.com>
2025-11-15 00:43:58 +0700

    Add MUI and setup provider

--- frontend/src/app/login/actions.ts
+++ frontend/src/app/login/actions.ts
@@ -1,16 +1,16 @@
 export const startGoogleLogin = () => {
-    const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!;
-    const REDIRECT_URI = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI!;
-    const SCOPE = ['openid', 'email', 'profile'].join(' ');
+  const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!;
+  const REDIRECT_URI = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI!;
+  const SCOPE = ['openid', 'email', 'profile'].join(' ');
 
-    const params = new URLSearchParams({
-        client_id: GOOGLE_CLIENT_ID,
-        redirect_uri: REDIRECT_URI,
-        response_type: 'code',
-        scope: SCOPE,
-        access_type: 'offline',
-        prompt: 'consent',
-    });
+  const params = new URLSearchParams({
+    client_id: GOOGLE_CLIENT_ID,
+    redirect_uri: REDIRECT_URI,
+    response_type: 'code',
+    scope: SCOPE,
+    access_type: 'offline',
+    prompt: 'consent',
+  });
 
-    globalThis.location.href = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
+  globalThis.location.href = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
 };
