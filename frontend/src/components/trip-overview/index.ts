3b68f7f759cb1b418bc9a30a0be46ff09ae05ce6
tori157 <puttinat2546@gmail.com>
2025-11-17 00:04:53 +0700

    Change Name

--- frontend/src/components/trip-overview/index.ts
+++ frontend/src/components/trip-overview/index.ts
@@ -1,5 +1,5 @@
 export { default as Header } from './header';
-export { default as DateRangePickerComponent } from './daterangepicker';
-export { default as TagSelector } from './TagSelector';
+export { default as DateRangePickerComponent } from './date-range-picker';
+export { default as TagSelector } from './tag-selector';
 export { default as OverviewTabs } from './overviewtabs';
 export { default as SectionCard } from './section-card';
