655acab7ec03a953732cbb72c61057665a8a4a94
ChinnaphatLoha <110705914+ChinnaphatLoha@users.noreply.github.com>
2025-11-28 14:22:14 +0700

    Refactor build-and-deploy.yml and Dockerfile

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,25 +1,28 @@
 import withPWAInit from 'next-pwa';
 
 const withPWA = withPWAInit({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
 });
 
+const basePath = process.env.BASE_PATH || '';
+
 const nextConfig = {
-  reactStrictMode: true,
-  typedRoutes: true,
-  output: 'standalone',
-  basePath: process.env.BASE_PATH || '',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-    ],
-  },
+    reactStrictMode: true,
+    typedRoutes: true,
+    output: 'standalone',
+    basePath,
+    assetPrefix: basePath,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+        ],
+    },
 };
 
 // @ts-expect-error: next-pwa types conflict with Next.js types
