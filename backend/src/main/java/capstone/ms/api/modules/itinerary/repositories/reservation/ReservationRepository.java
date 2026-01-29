c8b46dd4c099ce0f020e058f4a083deed41e3e97
ChinnaphatLoha <110705914+ChinnaphatLoha@users.noreply.github.com>
2026-01-29 21:01:34 +0700

    Fix GGMP enrichment for reservation

--- backend/src/main/java/capstone/ms/api/modules/itinerary/repositories/reservation/ReservationRepository.java
+++ backend/src/main/java/capstone/ms/api/modules/itinerary/repositories/reservation/ReservationRepository.java
@@ -1,7 +1,11 @@
 package capstone.ms.api.modules.itinerary.repositories.reservation;
 
+import capstone.ms.api.modules.itinerary.entities.Trip;
 import capstone.ms.api.modules.itinerary.entities.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+    List<Reservation> findByTrip(Trip trip);
 }
