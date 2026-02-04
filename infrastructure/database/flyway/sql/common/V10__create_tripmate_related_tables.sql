6630be6fe6cfcfa8c1666c44c9c7d14b29a6cc97
namfonn02 <Lalita.benj@kmutt.ac.th>
2026-02-04 13:37:56 +0700

    fix tripmate migration quoting issue

--- infrastructure/database/flyway/sql/common/V10__create_tripmate_related_tables.sql
+++ infrastructure/database/flyway/sql/common/V10__create_tripmate_related_tables.sql
@@ -1,18 +1,18 @@
-CREATE TABLE tripmate (
+CREATE TABLE "tripmate" (
     user_id       INT    NOT NULL,
     trip_id       INT    NOT NULL,
 
     CONSTRAINT pk_tripmate PRIMARY KEY (user_id, trip_id),
-    CONSTRAINT fk_tripmate_user FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE,
-    CONSTRAINT fk_tripmate_trip FOREIGN KEY (trip_id) REFERENCES trip (trip_id) ON DELETE CASCADE
+    CONSTRAINT fk_tripmate_user FOREIGN KEY (user_id) REFERENCES "user" (user_id) ON DELETE CASCADE,
+    CONSTRAINT fk_tripmate_trip FOREIGN KEY (trip_id) REFERENCES "trip" (trip_id) ON DELETE CASCADE
 );
 
-CREATE TABLE pending_tripmate_invitation (
+CREATE TABLE "pending_tripmate_invitation" (
     invitation_id SERIAL PRIMARY KEY,
     user_id       INT    NOT NULL,
     trip_id       INT    NOT NULL,
 
-    CONSTRAINT fk_pending_user FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE,
-    CONSTRAINT fk_pending_trip FOREIGN KEY (trip_id) REFERENCES trip (trip_id) ON DELETE CASCADE,
+    CONSTRAINT fk_pending_user FOREIGN KEY (user_id) REFERENCES "user" (user_id) ON DELETE CASCADE,
+    CONSTRAINT fk_pending_trip FOREIGN KEY (trip_id) REFERENCES "trip" (trip_id) ON DELETE CASCADE,
     CONSTRAINT uq_pending_tripmate_invitation UNIQUE (user_id, trip_id)
 );
