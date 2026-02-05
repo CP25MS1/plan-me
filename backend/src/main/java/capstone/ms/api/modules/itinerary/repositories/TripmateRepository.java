6f68f9f28836a9dc061350dff2a3afe06b988c55
namfonn02 <Lalita.benj@kmutt.ac.th>
2026-02-05 22:50:34 +0700

    Add DTOs and repositories for tripmate invitations

---
+++ backend/src/main/java/capstone/ms/api/modules/itinerary/repositories/TripmateRepository.java
@@ -0,0 +1,8 @@
+package capstone.ms.api.modules.itinerary.repositories;
+
+import capstone.ms.api.modules.itinerary.entities.Tripmate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TripmateRepository extends JpaRepository<Tripmate, Integer> {
+    boolean existsTripmateByTripIdAndUserId(Integer tripId, Integer userId);
+}
