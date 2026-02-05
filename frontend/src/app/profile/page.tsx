6a01ca1743dcb882c6e4a39b1010a989d5aa9f94
Tori157 <puttinat2546@gmail.com>
2026-02-05 20:28:09 +0700

    Change Search and AllTrip

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -7,24 +7,32 @@ import { LangSwitcher } from '@/components/profile';
 import { TruncatedTooltip } from '@/components/atoms';
 import { useAppSelector } from '@/store';
 import { useTranslation } from 'react-i18next';
+import { TripList } from '@/app/trip/all/page';
+import { useGetAllTrips } from '@/app/trip/all/hooks/use-get-all-trips';
+import { useRouter } from 'next/navigation';
 
 export default function ProfilePage() {
   const currentUser = useAppSelector((s) => s.profile.currentUser);
   const { t } = useTranslation('common');
+  const router = useRouter();
+
+  const { data: trips, isLoading, isError } = useGetAllTrips();
+
+  const handleTripClick = (tripId: number) => {
+    router.push(`/trip/${tripId}?tab=overview`);
+  };
 
   if (!currentUser) {
     return <FullPageLoading />;
   }
 
   return (
-    <div className="relative flex flex-col items-center px-6 h-[calc(100vh-64px)] overflow-hidden">
-      <button className="absolute top-4 right-4 flex items-center rounded-full shadow-sm">
-        <div className="relative flex items-center justify-center">
-          <LangSwitcher />
-        </div>
+    <div className="relative flex flex-col px-6 min-h-[calc(100vh-64px)] overflow-y-auto pb-7">
+      {' '}
+      <button className="absolute top-4 right-4 flex items-center rounded-full shadow-sm z-10">
+        <LangSwitcher />
       </button>
-
-      <div className="flex flex-col items-center justify-start h-full pt-5">
+      <div className="flex flex-col items-center pt-5 shrink-0">
         <h1 className="text-xl font-semibold text-black mb-8">
           <TruncatedTooltip className="max-w-1/2" text={currentUser?.username || 'User'} />
         </h1>
@@ -41,23 +49,27 @@ export default function ProfilePage() {
         <h2 className="text-xl font-semibold text-gray-800 mb-3">
           {currentUser?.username || 'User'}
         </h2>
+
         <p className="text-gray-500 text-sm mb-2">{currentUser?.email || '-'}</p>
 
         <div className="w-3/4 border-b border-gray-200 my-5" />
 
         <div className="flex items-center justify-center gap-10 text-center">
-          <Link href="/profile/followers" className="cursor-pointer">
+          <Link href="/profile/followers">
             <p className="text-sm font-semibold text-gray-800">{currentUser?.followers.length}</p>
             <p className="text-sm text-gray-500">{t('profile.followers')}</p>
           </Link>
 
           <div className="h-8 w-px bg-gray-300" />
 
-          <Link href="/profile/following" className="cursor-pointer">
+          <Link href="/profile/following">
             <p className="text-sm font-semibold text-gray-800">{currentUser?.following.length}</p>
             <p className="text-sm text-gray-500">{t('profile.following')}</p>
           </Link>
         </div>
+      </div>
+      <div className="w-full pb-6">
+        <TripList trips={trips} loading={isLoading} error={isError} onTripClick={handleTripClick} />
       </div>
     </div>
   );
