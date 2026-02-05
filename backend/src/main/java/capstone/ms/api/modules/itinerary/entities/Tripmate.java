9c0c19620e115d054a4e22de874e6805bfe09933
namfonn02 <Lalita.benj@kmutt.ac.th>
2026-02-05 22:50:00 +0700

    Add PendingTripmateInvitation and Tripmate entities

---
+++ backend/src/main/java/capstone/ms/api/modules/itinerary/entities/Tripmate.java
@@ -0,0 +1,30 @@
+package capstone.ms.api.modules.itinerary.entities;
+
+import capstone.ms.api.modules.user.entities.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tripmate")
+public class Tripmate {
+    @EmbeddedId
+    private TripmateId id;
+
+    @MapsId("userId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @MapsId("tripId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+}
