f699f2ad5be867b91594d9168b5c1a5e7a1a3dd6
Tori157 <puttinat2546@gmail.com>
2026-02-05 20:28:09 +0700

    Change Search and AllTrip

--- frontend/next-i18next.config.mjs
+++ frontend/next-i18next.config.mjs
@@ -5,7 +5,7 @@ export default {
     locales: ['en', 'th'],
     localeDetection: false,
   },
-  ns: ['common', 'trip_create', 'profile', 'trip_overview', 'trip_all'],
+  ns: ['common', 'trip_create', 'profile', 'trip_overview', 'search'],
   defaultNS: 'common',
   localPath: '/capstone25/cp25ms1/locales',
   // localePath: './public/locales',
