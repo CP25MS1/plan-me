12522f8ddcaf981c8967af6a094a494270ec1da0
namfonn02 <Lalita.benj@kmutt.ac.th>
2026-02-06 00:55:17 +0700

    Refactor tripmate DTOs and entities to organize under tripmate package

--- backend/src/main/java/capstone/ms/api/modules/itinerary/entities/tripmate/PendingTripmateInvitation.java
+++ backend/src/main/java/capstone/ms/api/modules/itinerary/entities/tripmate/PendingTripmateInvitation.java
@@ -1,27 +1,33 @@
-package capstone.ms.api.modules.itinerary.entities;
+package capstone.ms.api.modules.itinerary.entities.tripmate;
 
+import capstone.ms.api.modules.itinerary.entities.Trip;
 import capstone.ms.api.modules.user.entities.User;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 @Getter
+@NoArgsConstructor
 @Setter
 @Entity
-@Table(name = "tripmate")
-public class Tripmate {
-    @EmbeddedId
-    private TripmateId id;
+@Table(name = "pending_tripmate_invitation")
+public class PendingTripmateInvitation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "invitation_id", nullable = false)
+    private Integer id;
 
-    @MapsId("userId")
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @MapsId("tripId")
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "trip_id", nullable = false)
--- backend/src/main/java/capstone/ms/api/modules/itinerary/entities/tripmate/Tripmate.java
+++ backend/src/main/java/capstone/ms/api/modules/itinerary/entities/tripmate/Tripmate.java
@@ -1,32 +1,28 @@
-package capstone.ms.api.modules.itinerary.entities;
+package capstone.ms.api.modules.itinerary.entities.tripmate;
 
+import capstone.ms.api.modules.itinerary.entities.Trip;
 import capstone.ms.api.modules.user.entities.User;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 @Getter
-@NoArgsConstructor
 @Setter
 @Entity
-@Table(name = "pending_tripmate_invitation")
-public class PendingTripmateInvitation {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "invitation_id", nullable = false)
-    private Integer id;
+@Table(name = "tripmate")
+public class Tripmate {
+    @EmbeddedId
+    private TripmateId id;
 
-    @NotNull
+    @MapsId("userId")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @NotNull
+    @MapsId("tripId")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "trip_id", nullable = false)
