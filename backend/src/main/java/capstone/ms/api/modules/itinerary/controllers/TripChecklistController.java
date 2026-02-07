6683251dad769168bf4f9f0fc9f7b8f6235490db
ChinnaphatLoha <110705914+ChinnaphatLoha@users.noreply.github.com>
2026-02-08 03:11:14 +0700

    Add read/update/delete trip's checklist endpoints

--- backend/src/main/java/capstone/ms/api/modules/itinerary/controllers/TripChecklistController.java
+++ backend/src/main/java/capstone/ms/api/modules/itinerary/controllers/TripChecklistController.java
@@ -2,6 +2,7 @@ package capstone.ms.api.modules.itinerary.controllers;
 
 import capstone.ms.api.modules.itinerary.dto.checklist.CreateTripChecklistRequest;
 import capstone.ms.api.modules.itinerary.dto.checklist.TripChecklistDto;
+import capstone.ms.api.modules.itinerary.dto.checklist.UpdateTripChecklistRequest;
 import capstone.ms.api.modules.itinerary.services.TripChecklistService;
 import capstone.ms.api.modules.user.entities.User;
 import jakarta.validation.Valid;
@@ -11,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("/trips/{tripId}/checklist-items")
@@ -18,13 +21,41 @@ public class TripChecklistController {
     private final TripChecklistService service;
 
     @PostMapping
-    public ResponseEntity<TripChecklistDto> createTripChecklist
-            (
-                    final @PathVariable Integer tripId,
-                    final @Valid @RequestBody CreateTripChecklistRequest request,
-                    final @AuthenticationPrincipal User currentUser
-            ) {
+    public ResponseEntity<TripChecklistDto> createTripChecklist(
+            final @PathVariable Integer tripId,
+            final @Valid @RequestBody CreateTripChecklistRequest request,
+            final @AuthenticationPrincipal User currentUser
+    ) {
         final TripChecklistDto createdChecklistItem = service.createTripChecklist(tripId, request, currentUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdChecklistItem);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TripChecklistDto>> getTripChecklist(
+            final @PathVariable Integer tripId,
+            final @AuthenticationPrincipal User currentUser
+    ) {
+        return ResponseEntity.ok(service.getTripChecklist(tripId, currentUser));
+    }
+
+    @PatchMapping("/{itemId}")
+    public ResponseEntity<TripChecklistDto> updateTripChecklist(
+            final @PathVariable Integer tripId,
+            final @Valid @RequestBody UpdateTripChecklistRequest request,
+            final @AuthenticationPrincipal User currentUser,
+            final @PathVariable Integer itemId
+    ) {
+        final TripChecklistDto updatedChecklistItem = service.updateTripChecklist(tripId, request, currentUser, itemId);
+        return ResponseEntity.ok(updatedChecklistItem);
+    }
+
+    @DeleteMapping("/{itemId}")
+    public ResponseEntity<Void> deleteTripChecklist(
+            final @PathVariable Integer tripId,
+            final @AuthenticationPrincipal User currentUser,
+            final @PathVariable Integer itemId
+    ) {
+        service.deleteTripChecklist(tripId, currentUser, itemId);
+        return ResponseEntity.noContent().build();
     }
 }
--- backend/src/main/java/capstone/ms/api/modules/itinerary/services/TripChecklistService.java
+++ backend/src/main/java/capstone/ms/api/modules/itinerary/services/TripChecklistService.java
@@ -1,12 +1,16 @@
 package capstone.ms.api.modules.itinerary.services;
 
+import capstone.ms.api.common.exceptions.BadRequestException;
 import capstone.ms.api.common.exceptions.ForbiddenException;
+import capstone.ms.api.common.exceptions.NotFoundException;
 import capstone.ms.api.modules.itinerary.dto.checklist.CreateTripChecklistRequest;
 import capstone.ms.api.modules.itinerary.dto.checklist.TripChecklistDto;
+import capstone.ms.api.modules.itinerary.dto.checklist.UpdateTripChecklistRequest;
 import capstone.ms.api.modules.itinerary.entities.TripChecklist;
 import capstone.ms.api.modules.itinerary.mappers.ChecklistMapper;
 import capstone.ms.api.modules.itinerary.repositories.TripChecklistRepository;
 import capstone.ms.api.modules.user.entities.User;
+import capstone.ms.api.modules.user.services.UserService;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,6 +25,7 @@ public class TripChecklistService {
     private final ChecklistMapper mapper;
     private final TripAccessService tripAccessService;
     private final TripResourceService tripResourceService;
+    private final UserService userService;
 
     @Transactional
     public TripChecklistDto createTripChecklist(
@@ -29,32 +34,111 @@ public class TripChecklistService {
             final User user
     ) {
         final var trip = tripResourceService.getTripOrThrow(tripId);
-        if (!tripAccessService.hasTripmateLevelAccess(user, tripId)) {
-            throw new ForbiddenException(TRIP_FORBIDDEN_KEY);
-        }
+        assertTripmateAccess(user, tripId);
 
-        var newChecklistItem = new TripChecklist();
+        TripChecklist newChecklistItem = new TripChecklist();
         newChecklistItem.setTrip(trip);
         newChecklistItem.setCreatedBy(user);
         newChecklistItem.setName(request.getName());
         newChecklistItem.setCompleted(false);
 
-        var createdChecklistItem = repository.save(newChecklistItem);
+        final TripChecklist createdChecklistItem = repository.save(newChecklistItem);
 
         return mapper.toDto(createdChecklistItem);
     }
 
-    public List<TripChecklistDto> getTripChecklist() {
-        return null;
+    public List<TripChecklistDto> getTripChecklist(final Integer tripId, final User user
+    ) {
+        assertTripmateAccess(user, tripId);
+        return repository.findAllByTripId(tripId).stream().map(mapper::toDto).toList();
     }
 
     @Transactional
-    public TripChecklistDto updateTripChecklist() {
-        return null;
+    public TripChecklistDto updateTripChecklist(
+            final Integer tripId,
+            final UpdateTripChecklistRequest request,
+            final User user,
+            final Integer itemId
+    ) {
+        assertTripmateAccess(user, tripId);
+
+        final TripChecklist checklist = repository.findById(itemId).orElseThrow(() -> new NotFoundException("404"));
+
+        if (request.getName() != null) {
+            validateChecklistName(request.getName());
+            checklist.setName(request.getName());
+        }
+
+        if (request.isAssigneePresent()) {
+            patchAssignee(checklist, request.getAssigneeId(), tripId, user);
+        }
+
+        if (request.getCompleted() != null) {
+            assertCanUpdateCompleted(checklist, user);
+            checklist.setCompleted(request.getCompleted());
+        }
+
+        final TripChecklist updatedChecklistItem = repository.save(checklist);
+        return mapper.toDto(updatedChecklistItem);
     }
 
     @Transactional
-    public void deleteTripChecklist() {
-        // document why this method is empty
+    public void deleteTripChecklist(final Integer tripId, final User user, final Integer itemId) {
+        assertTripmateAccess(user, tripId);
+        repository.deleteById(itemId);
     }
+
+    private void assertTripmateAccess(User user, Integer tripId) {
+        if (!tripAccessService.hasTripmateLevelAccess(user, tripId)) {
+            throw new ForbiddenException(TRIP_FORBIDDEN_KEY);
+        }
+    }
+
+    private void validateChecklistName(String name) {
+        if (name == null) return;
+
+        if (name.isBlank()) {
+            throw new BadRequestException("400", "tripChecklist.blankName");
+        }
+    }
+
+    private void assertCanUpdateCompleted(final TripChecklist checklist, final User user) {
+        final User assignee = checklist.getAssignee();
+
+        // NOTE: if no assignee, everyone in the trip can update completed status.
+        if (assignee != null && !assignee.getId().equals(user.getId())) {
+            throw new ForbiddenException("tripChecklist.completeForbidden");
+        }
+    }
+
+    private void patchAssignee(
+            final TripChecklist checklist,
+            final Integer assigneeId,
+            final Integer tripId,
+            final User actor
+    ) {
+        // unassign
+        if (assigneeId == null) {
+            if (checklist.getAssignee() != null) {
+                checklist.setAssignee(null);
+                checklist.setAssignedBy(actor);
+                checklist.setCompleted(false); // should set to default after unassign
+            }
+            return;
+        }
+
+        final User assignee = userService.getUserOrThrow(assigneeId);
+
+        if (!tripAccessService.hasTripmateLevelAccess(assignee, tripId)) {
+            throw new ForbiddenException(TRIP_FORBIDDEN_KEY);
+        }
+
+        if (checklist.getAssignee() != null && checklist.getAssignee().getId().equals(assignee.getId())) {
+            return;
+        }
+
+        checklist.setAssignee(assignee);
+        checklist.setAssignedBy(actor);
+    }
+
 }
