d139818c23ad7b091463d77828665b7c6a25e1fc
Tori157 <puttinat2546@gmail.com>
2026-02-07 14:45:06 +0700

    Resolved

--- frontend/src/app/trip/[tripId]/hooks/use-update-trip-overview.ts
+++ frontend/src/app/trip/[tripId]/hooks/use-update-trip-overview.ts
@@ -1,9 +1,16 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { updateTripOverview, UpsertTrip } from '@/api/trips';
 
 export const useUpdateTripOverview = (tripId: number) => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (tripInfo: UpsertTrip) => updateTripOverview(tripId, tripInfo),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['all-trips'],
+      });
+    },
   });
 };
 
