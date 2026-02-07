915d00fbdf99dd006a9094f0daa9f41bebbd369c
namfonn02 <Lalita.benj@kmutt.ac.th>
2026-02-08 01:49:55 +0700

    Implement mark as read functionality for notifications

--- backend/src/main/java/capstone/ms/api/modules/itinerary/repositories/NotificationRepository.java
+++ backend/src/main/java/capstone/ms/api/modules/itinerary/repositories/NotificationRepository.java
@@ -4,7 +4,9 @@ import capstone.ms.api.modules.itinerary.entities.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
     List<Notification> findByReceiverUserIdOrderByCreatedAtDesc(Integer receiverUserId);
+    Optional<Notification> findByIdAndReceiverUserId(Integer id, Integer receiverUserId);
 }
