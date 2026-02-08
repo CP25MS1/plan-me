9938dfe27d2c9c6485a630a5d8dc41b70912fdc0
Tori157 <puttinat2546@gmail.com>
2026-02-08 20:40:26 +0700

    API

--- frontend/src/app/trip/[tripId]/@overview/hooks/invite/use-get-friends.ts
+++ frontend/src/app/trip/[tripId]/@overview/hooks/invite/use-get-friends.ts
@@ -2,28 +2,12 @@
 
 import { useQuery } from '@tanstack/react-query';
 import { getFriends } from '@/api/invite/api';
-import { useAppDispatch } from '@/store';
-import { setFriends } from '@/store/invite-slice';
 import { PublicUserInfo } from '@/api/users';
-import { useEffect } from 'react';
 
 export const FRIENDS_QUERY_KEY = ['friends'] as const;
 
-export const useGetFriends = () => {
-  const dispatch = useAppDispatch();
-
-  const query = useQuery<PublicUserInfo[]>({
+export const useGetFriends = () =>
+  useQuery<PublicUserInfo[]>({
     queryKey: FRIENDS_QUERY_KEY,
     queryFn: getFriends,
   });
-
-  const { data } = query;
-
-  useEffect(() => {
-    if (data) {
-      dispatch(setFriends(data));
-    }
-  }, [data, dispatch]);
-
-  return query;
-};
