9cec2bd51c4683d6c025f4aacfc96fc0b94156a0
ChinnaphatLoha <110705914+ChinnaphatLoha@users.noreply.github.com>
2026-02-12 16:56:15 +0700

    Optimize Google Maps Places API (New) cost by limit qouta of GetPhotoMediaRequest

--- backend/src/main/java/capstone/ms/api/modules/google_maps/repositories/GoogleMapPlaceRepository.java
+++ backend/src/main/java/capstone/ms/api/modules/google_maps/repositories/GoogleMapPlaceRepository.java
@@ -2,7 +2,18 @@ package capstone.ms.api.modules.google_maps.repositories;
 
 import capstone.ms.api.modules.google_maps.entities.GoogleMapPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface GoogleMapPlaceRepository extends JpaRepository<GoogleMapPlace, String> {
 
+    @Query(value = """
+            SELECT *
+            FROM google_map_place
+            WHERE search_vector @@ plainto_tsquery('simple', :q)
+            ORDER BY rating DESC
+            """, nativeQuery = true)
+    List<GoogleMapPlace> searchFullText(@Param("q") String query);
 }
