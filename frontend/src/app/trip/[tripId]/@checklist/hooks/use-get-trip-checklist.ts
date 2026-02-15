b62d66a7b329469f4901ec10cb2be78d114f8c83
Tori157 <puttinat2546@gmail.com>
2026-02-15 23:50:40 +0700

    Fix Bug - Edit Reservation - Dialog Member - Search Daily Reservation - Checklist type - Checklist UI - Close Icon In Email Preview - Snackbar Upload Wrong Type

--- frontend/src/app/trip/[tripId]/@checklist/hooks/use-get-trip-checklist.ts
+++ frontend/src/app/trip/[tripId]/@checklist/hooks/use-get-trip-checklist.ts
@@ -2,19 +2,20 @@ import { useQuery } from '@tanstack/react-query';
 import { getChecklistItems } from '@/api/checklist/api';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { setTripChecklist } from '@/store/trip-detail-slice';
+import { setTripChecklist, resetTripChecklist } from '@/store/trip-detail-slice';
 import { RootState } from '@/store';
 
 export const useGetTripChecklist = (tripId: number) => {
   const dispatch = useDispatch();
-
   const itemsInStore = useSelector((s: RootState) => s.tripDetail.checklist);
+
+  useEffect(() => {
+    dispatch(resetTripChecklist());
+  }, [tripId, dispatch]);
 
   const query = useQuery({
     queryKey: ['trip-checklist', tripId],
     queryFn: () => getChecklistItems(tripId),
-
-    enabled: itemsInStore.length === 0,
   });
 
   useEffect(() => {
