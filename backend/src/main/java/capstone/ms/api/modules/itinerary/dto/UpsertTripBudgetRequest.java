b726820f6e23c6a8424bdeb7e53b50c9298c50c9
namfonn02 <Lalita.benj@kmutt.ac.th>
2026-03-11 20:46:34 +0700

    Replace String with BigDecimal for trip budget request

--- backend/src/main/java/capstone/ms/api/modules/itinerary/dto/UpsertTripBudgetRequest.java
+++ backend/src/main/java/capstone/ms/api/modules/itinerary/dto/UpsertTripBudgetRequest.java
@@ -1,16 +1,19 @@
 package capstone.ms.api.modules.itinerary.dto;
 
 import capstone.ms.api.common.annotations.ErrorMessage;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
 @ErrorMessage(messageKey = "400")
 public class UpsertTripBudgetRequest {
     @NotNull(message = "Trip budget cannot be null.")
-    @Pattern(regexp = "^-?\\d+(\\.\\d{1,2})?$", message = "Trip budget must be a decimal number with up to 2 decimal places.")
-    private final String totalBudget;
+    @Digits(integer = 12, fraction = 2, message = "Trip budget must have up to 2 decimal places.")
+    private final BigDecimal totalBudget;
+
 }
