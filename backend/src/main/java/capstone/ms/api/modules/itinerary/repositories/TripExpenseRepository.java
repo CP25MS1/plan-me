5131102e42db18fb52142f226ccbf438fb0e0398
namfonn02 <Lalita.benj@kmutt.ac.th>
2026-03-14 00:42:04 +0700

    Implement API to view trip expenses list

--- backend/src/main/java/capstone/ms/api/modules/itinerary/repositories/TripExpenseRepository.java
+++ backend/src/main/java/capstone/ms/api/modules/itinerary/repositories/TripExpenseRepository.java
@@ -3,8 +3,11 @@ package capstone.ms.api.modules.itinerary.repositories;
 import capstone.ms.api.modules.itinerary.entities.expense.TripExpense;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TripExpenseRepository extends JpaRepository<TripExpense, Integer> {
     Optional<TripExpense> findByIdAndTripId(Integer expenseId, Integer tripId);
+
+    List<TripExpense> findByTripIdOrderBySpentAtDesc(Integer tripId);
 }
