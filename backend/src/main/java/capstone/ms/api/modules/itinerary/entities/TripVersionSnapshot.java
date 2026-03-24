509224a88effa67cb47376c0dd9eda145757d0ae
namfonn02 <Lalita.benj@kmutt.ac.th>
2026-03-24 22:02:48 +0700

    Remove unused method and import

--- backend/src/main/java/capstone/ms/api/modules/itinerary/entities/TripVersionSnapshot.java
+++ backend/src/main/java/capstone/ms/api/modules/itinerary/entities/TripVersionSnapshot.java
@@ -1,6 +1,5 @@
 package capstone.ms.api.modules.itinerary.entities;
 
-import capstone.ms.api.common.converters.MapToJsonConverter;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
