f08dc96f633e6b98ff55613d77e480dd307ff609
namfonn02 <Lalita.benj@kmutt.ac.th>
2026-03-24 22:02:19 +0700

    Implement create trip version API

---
+++ backend/src/main/java/capstone/ms/api/modules/itinerary/repositories/TripVersionRepository.java
@@ -0,0 +1,23 @@
+package capstone.ms.api.modules.itinerary.repositories;
+
+import capstone.ms.api.modules.itinerary.entities.TripVersion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TripVersionRepository extends JpaRepository<TripVersion, Integer> {
+    int countByTripId(Integer tripId);
+
+    boolean existsByTripIdAndVersionName(Integer tripId, String versionName);
+
+    Optional<TripVersion> findByTripIdAndIsCurrentTrue(Integer tripId);
+
+    @Modifying
+    @Query("UPDATE TripVersion v SET v.isCurrent = false WHERE v.trip IS NOT NULL AND v.trip.id = :tripId AND v.isCurrent = true")
+    void clearCurrentForTrip(@Param("tripId") Integer tripId);
+}
