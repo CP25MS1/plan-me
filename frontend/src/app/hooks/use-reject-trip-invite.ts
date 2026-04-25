2ee9df4ee86fb9316e163a78ee64b90d863da0a0
ChinnaphatLoha <110705914+ChinnaphatLoha@users.noreply.github.com>
2026-04-25 19:25:19 +0700

    Fix gaps in previous changes

--- frontend/src/app/hooks/use-reject-trip-invite.ts
+++ frontend/src/app/hooks/use-reject-trip-invite.ts
@@ -3,9 +3,20 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { rejectTripInvite } from '@/api/invite/api';
+import { useSnackbar } from '@/components/common/snackbar/snackbar';
+import { useTranslation } from 'react-i18next';
 
 export const useRejectTripInvite = (tripId: number) => {
+  const { t } = useTranslation('common');
+  const { showSuccess, showError } = useSnackbar();
+
   return useMutation({
     mutationFn: ({ inviteId }: { inviteId: number }) => rejectTripInvite(tripId, inviteId),
+    onSuccess: () => {
+      showSuccess(t('notification.success.reject_invite'));
+    },
+    onError: () => {
+      showError(t('notification.error.reject_invite'));
+    },
   });
 };
--- frontend/src/app/invitations/use-respond-to-invitation.ts
+++ frontend/src/app/invitations/use-respond-to-invitation.ts
@@ -1,9 +1,20 @@
 import { RespondInvitationRequest, respondToInvitation } from '@/api/invite';
+import { useSnackbar } from '@/components/common/snackbar/snackbar';
 import { useMutation } from '@tanstack/react-query';
+import { useTranslation } from 'react-i18next';
 
 export const useRespondToInvitation = () => {
+  const { t } = useTranslation('common');
+  const { showSuccess, showError } = useSnackbar();
+
   return useMutation({
     mutationFn: ({ tripId, request }: { tripId: number; request: RespondInvitationRequest }) =>
       respondToInvitation(tripId, request),
+    onSuccess: () => {
+      showSuccess(t('notification.success.respond_invite'));
+    },
+    onError: () => {
+      showError(t('notification.error.respond_invite'));
+    },
   });
 };
